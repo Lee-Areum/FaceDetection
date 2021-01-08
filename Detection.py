ebf7ce497a5fd9d559eb46506048b1a8093079d5
aream <arlee957790@gmail.com>
2021-01-08 10:33:20 +0900

    21.01.08 fear 제거 및 코드 수정

--- Detection.py
+++ Detection.py
@@ -1,14 +1,28 @@
 #필요한 library import
 import numpy as np
+import glob
 import PIL
 from PIL import Image, ImageDraw,ImageFont
 import cv2
 from tensorflow.keras.models import load_model
 import os
+import dlib
+
+# create list for landmarks
+ALL = list(range(0, 68))
+RIGHT_EYEBROW = list(range(17, 22))
+LEFT_EYEBROW = list(range(22, 27))
+RIGHT_EYE = list(range(36, 42))
+LEFT_EYE = list(range(42, 48))
+NOSE = list(range(27, 36))
+MOUTH_OUTLINE = list(range(48, 61))
+MOUTH_INNER = list(range(61, 68))
+JAWLINE = list(range(0, 17))
 
 class detection:
     i = 0
-    tag = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']  # 감정 라벨들
+#    tag = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']  # 감정 라벨들
+    tag = ['angry', 'disgust', 'happy', 'sad', 'surprise', 'neutral']  # 감정 라벨들
 
     def __init__(self):
         pass
@@ -21,22 +35,56 @@ class detection:
 
     def FaceDetection(self,image,type):
         # 얼굴 개수 알려주는 코드
+        """ dlib 사용전
         xml = 'haarcascades/haarcascade_frontalface_default.xml'
         face_cascade = cv2.CascadeClassifier(xml)
 
         gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY) #gray scale로 변환
         faces = face_cascade.detectMultiScale(gray,1.05,5)
         print('Number of faces detected: ' + str(len(faces))) #얼굴 개수 출력
+        """
+        detector = dlib.get_frontal_face_detector()
+        #predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+        # resize the video
+
+        #image = cv2.resize(image,dsize=(640, 480), interpolation=cv2.INTER_AREA)
+        img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Get faces (up-sampling=1)
+        face_detector = detector(img_gray, 1)
+        # the number of face detected
+        print("The number of faces detected : {}".format(len(face_detector)))
+
+        for face in face_detector:
+            # face wrapped with rectangle
+            cv2.rectangle(image, (face.left(), face.top()), (face.right(), face.bottom()),
+                          (0, 0, 255), 3)
+            cutimg = self.Cut_Image(face.left(),face.top(),face.right(),face.bottom(),img_gray) #얼굴모양으로 자르는 함수
+#                print('cuting image')
+            image = self.AddPercentage(image,cutimg,face.left(),face.top(),face.bottom()/10) #각 tag별 확률을 적는 함수
+#                print('add percentage')
+
+            # make prediction and transform to numpy array
+            # landmarks = predictor(image, face)  # 얼굴에서 68개 점 찾기
+            #
+            # # create list to contain landmarks
+            # landmark_list = []
+            #
+            # # append (x, y) in landmark_list
+            # for p in landmarks.parts():
+            #     landmark_list.append([p.x, p.y])
+            #     cv2.circle(image, (p.x, p.y), 2, (0, 255, 0), -1)
 
         # 이미지에 박스 침
+        """ dlib 사용전
         if len(faces):
             for (x, y, w, h) in faces:
                 cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 cutimg = self.Cut_Image(x,y,w,h,gray) #얼굴모양으로 자르는 함수
-                print('cuting image')
+#                print('cuting image')
                 image = self.AddPercentage(image,cutimg,x,y,h/10) #각 tag별 확률을 적는 함수
-                print('add percentage')
-
+#                print('add percentage')
+        """
         if type == 1:   #실시간 일 경우 type = 1
             cv2.imshow('result', image)
         elif type == 0:    #테스트 이미지 일 경우 type = 0
@@ -48,45 +96,49 @@ class detection:
         image = np.array(image, dtype='float32')
         image /= 255
         model = load_model('learned_model_10.h5') #h5 로드
-        print("end 전처리")
+#        print("end 전처리")
         pred_array = model.predict(image) #predict 호출
-        print("end prediction")
+#        print("end prediction")
         return pred_array
 
     def AddPercentage(self,img, cutimg,x,y,n): #각 감정의 확률을 이미지에 넣는 변수
-        print("addpercentage")
+#        print("addpercentage")
         cutimg = np.stack((cutimg,)*3, axis = -1)
         cutimg = cv2.resize(cutimg, (48,48))
         cutimg = cutimg.reshape(1,48,48,3)
         pred_array = self.predict_image(cutimg)
         result = np.argmax(pred_array) #가장 확률이 높은 결과 저장
         score = float("%0.2f" % (max(pred_array[0]) * 100))  #페센트 저장
-        print(f'Result: {result}, Score: {score}')
+        # if(score < 55):
+        #     result = 5
+ #       print(f'Result: {result}, Score: {score}')
         fontsFolder = 'Font/' #폰트가 저장된 위치
-        selectedFont = ImageFont.truetype(os.path.join(fontsFolder, 'ELAND 초이스 B.ttf'), int(n))  # 폰트 경로와 사이즈 지정
+        selectedFont = ImageFont.truetype(os.path.join(fontsFolder, 'ELAND 초이스 B.ttf'), int(n/2))  # 폰트 경로와 사이즈 지정
         img2 = Image.fromarray(img) #배열 형태의 img를 이미지 형태로 변경
         draw = ImageDraw.Draw(img2)
 
-        print("라벨 그리기 시작")
+#        print("라벨 그리기 시작")
         label =""
-        for i in range(7): # label 생성
+        for i in range(6): # label 생성
             label += (self.tag[i]+" : "+str(float("%0.2f" % (pred_array[0][i] * 100)))+"\n")
-            print(f'predict :{self.tag[i]} score : {str(float("%0.2f" % (pred_array[0][i] * 100)))}')
+#            print(f'predict :{self.tag[i]} score : {str(float("%0.2f" % (pred_array[0][i] * 100)))}')
         # 이미지에 박스 침
-        draw.text((x-(n*7.0), y+10), label, fill="white", font=selectedFont, align='center') #글자를 더함
-        print('rute : EMOJI/'+str(result)+'.png')
-        emoji = cv2.imread('EMOJI/'+str(result)+'.png')
+        draw.text((x-(n*3.3), y+8), label, fill="white", font=selectedFont, align='center') #글자를 더함
+ #       print('rute : EMOJI/'+str(result)+'.png')
+        emoji = cv2.imread('EMOJI/'+
+                           str(result)+'.png')
         emoji = cv2.resize(emoji,(70,70),interpolation = cv2.INTER_AREA)
         emoji = Image.fromarray(emoji)
 
-        print("이모지 open")
+#        print("이모지 open")
         area = (int(n)+x,y-70)
-        print(f'area x:{x} y: {y-int(n)}')
+#        print(f'area x:{x} y: {y-int(n)}')
         img2.paste(emoji,area)
-        print('paste 완료')
+#        print('paste 완료')
 
         img2 = np.array(img2) #array형태로 이미지 변환
-        cv2.imwrite('image/addtest.jpg',img2) #이미지 저장
+        cv2.imwrite('image/cutimage/faceimage'+str(detection.i)+'.jpg',img2) #이미지 저장
+        detection.i += 1
         return img2
 
     def ImgReSize(self, img): #이미지 크기 변경하는 변수
@@ -95,26 +147,27 @@ class detection:
     def Cut_Image(self,x,y,w,h,img): #이미지를 잘라서 저장하는 변수
         img_trim = img[y:y+h,x:x+w]
         resized_img = self.ImgReSize(img_trim)
-        cv2.imwrite('image/cutimage/faceimage'+str(detection.i)+'.jpg',resized_img)
-        detection.i += 1
         return resized_img
         
     def TestRun(self):
         #테스트용 이미지 사용
-        image = cv2.imread('image/test.jpg')
-        self.FaceDetection(image, 0)
+        path = glob.glob('image/Test/*.jpg')
+        # image = cv2.imread('image/test.jpg')
+        for img in path:
+            image = cv2.imread(img)
+            self.FaceDetection(image, 0)
 
     def RealRun(self):
         cap = cv2.VideoCapture(0) #노트북 웹캠을 카메라로 사용
-        cap.set(3,640) #너비
-        cap.set(4,480) #높이
+ #       cap.set(3,1280) #너비
+  #      cap.set(4,960) #높이
 
         while(True):
             ret, frame = cap.read()
             frame = cv2.flip(frame, 1) #좌우 대칭 적용
 
             self.FaceDetection(frame, 1)
-            k = cv2.waitKey(1) & 0xff #딜레이 시간 결정
+            k = cv2.waitKey(1)# & 0xff #딜레이 시간 결정
 
             if k ==27 : #ESC 키를 누르면 종료
                 break
@@ -125,8 +178,8 @@ class detection:
 
 if __name__ == '__main__':
     cImg = detection() #클래스 생성
-    cImg.TestRun() #테스트 이미지용 실행
-    #cImg.RealRun() #실시간 이미지용 실행
+    #cImg.TestRun() #테스트 이미지용 실행
+    cImg.RealRun() #실시간 이미지용 실행
 
 
 
